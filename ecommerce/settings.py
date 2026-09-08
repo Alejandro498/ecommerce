d7b40347f47b63cc36f10655ad98488818ea7a87
Alejandro498 <luis.sanchez9105@alumnos.udg.mx>
2026-09-07 20:09:45 -0600

    Add USE_AWS flag to switch S3 and local storage

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -150,36 +150,36 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-# STATIC_URL = '/static/'
-# STATIC_ROOT = BASE_DIR /'static'
-# STATICFILES_DIRS = [
-#     'ecommerce/static'
-# ]
-AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
-AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
-AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME')
-AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
-AWS_S3_OBJECT_PARAMETERS = {
-    'CacheControl': 'max-age=86400',
-}
-AWS_S3_FILE_OVERWRITE = True
-AWS_DEFAULT_ACL = None
-AWS_QUERYSTRING_AUTH = False
-AWS_LOCATION = 'static'
+# USE_AWS=True  -> S3 for static/media (Elastic Beanstalk / production)
+# USE_AWS=False -> local disk (development). Restart the process after changing this.
+USE_AWS = config('USE_AWS', default=False, cast=bool)
 
 STATICFILES_DIRS = [
-    'ecommerce/static',
+    BASE_DIR / 'ecommerce' / 'static',
 ]
-STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-
-
-
-
-DEFAULT_FILE_STORAGE = 'ecommerce.media_storages.MediaStorage'
-
-MEDIA_URL = 'https://%s/media/' % AWS_S3_CUSTOM_DOMAIN
 MEDIA_ROOT = BASE_DIR / 'media'
+
+if USE_AWS:
+    AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
+    AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME')
+    AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+    AWS_S3_OBJECT_PARAMETERS = {
+        'CacheControl': 'max-age=86400',
+    }
+    AWS_S3_FILE_OVERWRITE = True
+    AWS_DEFAULT_ACL = None
+    AWS_QUERYSTRING_AUTH = False
+    AWS_LOCATION = 'static'
+
+    STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+    STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+    DEFAULT_FILE_STORAGE = 'ecommerce.media_storages.MediaStorage'
+    MEDIA_URL = 'https://%s/media/' % AWS_S3_CUSTOM_DOMAIN
+else:
+    STATIC_URL = '/static/'
+    STATIC_ROOT = BASE_DIR / 'static'
+    MEDIA_URL = '/media/'
 
 
 from django.contrib.messages import constants as messages
